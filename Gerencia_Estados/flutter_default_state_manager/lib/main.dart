3e69b96c048e97c5074ff643053c4e02b43567af
Gisele <you@example.com>
2021-10-02 23:31:04 -0300

    tela que carrega a page principal

--- Gerencia_Estados/flutter_default_state_manager/lib/main.dart
+++ Gerencia_Estados/flutter_default_state_manager/lib/main.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_default_state_manager/home_page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home: const HomePage(),
     );
   }
 }
