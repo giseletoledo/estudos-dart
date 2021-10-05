13cd71c22dd460d0b0fd3a46f70fd4d072a67e92
Gisele <you@example.com>
2021-10-04 22:30:08 -0300

    calculando e validando formulário

--- Gerencia_Estados/flutter_default_state_manager/lib/setState/imc_setstate_page.dart
+++ Gerencia_Estados/flutter_default_state_manager/lib/setState/imc_setstate_page.dart
@@ -1,6 +1,9 @@
+import 'dart:math';
+
 import 'package:currency_text_input_formatter/currency_text_input_formatter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_default_state_manager/widgets/imc_gauge.dart';
+import 'package:intl/intl.dart';
 
 class ImcSetstatePage extends StatefulWidget {
   const ImcSetstatePage({Key? key}) : super(key: key);
@@ -12,6 +15,20 @@ class ImcSetstatePage extends StatefulWidget {
 class _ImcSetstatePageState extends State<ImcSetstatePage> {
   final pesoEC = TextEditingController();
   final alturaEC = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+  var imc = 0.0;
+
+  void _calcularIMC({required double peso, required double altura}) async {
+    setState(() {
+      imc = 0;
+    });
+
+    await Future.delayed(Duration(seconds: 1));
+
+    setState(() {
+      imc = peso / pow(altura, 2);
+    });
+  }
 
   @override
   void dispose() {
@@ -27,46 +44,71 @@ class _ImcSetstatePageState extends State<ImcSetstatePage> {
         title: const Text('IMC SetState'),
       ),
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              const ImcGauge(
-                imc: 0,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                controller: pesoEC,
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(labelText: 'Peso'),
-                inputFormatters: [
-                  CurrencyTextInputFormatter(
-                      locale: 'pt_BR',
-                      symbol: '',
-                      turnOffGrouping: true,
-                      decimalDigits: 2)
-                ],
-              ),
-              TextFormField(
-                controller: alturaEC,
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(labelText: 'Altura'),
-                inputFormatters: [
-                  CurrencyTextInputFormatter(
-                      locale: 'pt_BR',
-                      symbol: '',
-                      turnOffGrouping: true,
-                      decimalDigits: 2)
-                ],
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              ElevatedButton(
-                  onPressed: () {}, child: const Text('Calcular IMC'))
-            ],
+        child: Form(
+          key: formKey,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                ImcGauge(imc: imc),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  controller: pesoEC,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(labelText: 'Peso'),
+                  inputFormatters: [
+                    CurrencyTextInputFormatter(
+                        locale: 'pt_BR',
+                        symbol: '',
+                        turnOffGrouping: true,
+                        decimalDigits: 2)
+                  ],
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Peso obrigatório';
+                    }
+                  },
+                ),
+                TextFormField(
+                  controller: alturaEC,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(labelText: 'Altura'),
+                  inputFormatters: [
+                    CurrencyTextInputFormatter(
+                        locale: 'pt_BR',
+                        symbol: '',
+                        turnOffGrouping: true,
+                        decimalDigits: 2)
+                  ],
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Altura obrigatória';
+                    }
+                  },
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      var formValid = formKey.currentState?.validate() ?? false;
+
+                      if (formValid) {
+                        var formatter = NumberFormat.simpleCurrency(
+                            locale: 'pt_BR', decimalDigits: 2);
+
+                        double peso = formatter.parse(pesoEC.text) as double;
+                        double altura =
+                            formatter.parse(alturaEC.text) as double;
+
+                        _calcularIMC(peso: peso, altura: altura);
+                      }
+                    },
+                    child: const Text('Calcular IMC'))
+              ],
+            ),
           ),
         ),
       ),
