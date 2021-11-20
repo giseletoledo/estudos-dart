fedf574eb01ef1025c50946eb63763ebbbe1a887
Gisele <you@example.com>
2021-11-20 13:23:54 -0300

    ValueNotifier

--- Gerencia_Estados/flutter_default_state_manager/lib/home_page.dart
+++ Gerencia_Estados/flutter_default_state_manager/lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_default_state_manager/setState/imc_setstate_page.dart';
+import 'package:flutter_default_state_manager/value_notifier/imc_value_notifier_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -25,7 +26,7 @@ class HomePage extends StatelessWidget {
               child: const Text('SetState'),
             ),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () => _gotoPage(context, ImcValueNotifierPage()),
               child: const Text('ValueNotifier'),
             ),
             ElevatedButton(
---
+++ Gerencia_Estados/flutter_default_state_manager/lib/value_notifier/imc_value_notifier_page.dart
@@ -0,0 +1,110 @@
+import 'dart:math';
+
+import 'package:currency_text_input_formatter/currency_text_input_formatter.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_default_state_manager/widgets/imc_gauge.dart';
+import 'package:intl/intl.dart';
+
+class ImcValueNotifierPage extends StatefulWidget {
+  const ImcValueNotifierPage({Key? key}) : super(key: key);
+
+  @override
+  State<ImcValueNotifierPage> createState() => _ImcValueNotifierPageState();
+}
+
+class _ImcValueNotifierPageState extends State<ImcValueNotifierPage> {
+  final pesoEC = TextEditingController();
+  final alturaEC = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+  var imc = ValueNotifier(0.0);
+
+  @override
+  void dispose() {
+    pesoEC.dispose();
+    alturaEC.dispose();
+    super.dispose();
+  }
+
+  Future<void> _calcularIMC(
+      {required double peso, required double altura}) async {
+    imc.value = 0;
+    await Future.delayed(Duration(seconds: 1));
+    imc.value = peso / pow(altura, 2);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('-----------------------------');
+    print('BUILD_TELA');
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('IMC ValueNotifier'),
+      ),
+      body: SingleChildScrollView(
+        child: Form(
+          key: formKey,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                ValueListenableBuilder<double>(
+                    valueListenable: imc,
+                    builder: (_, imcValue, __) {
+                      return ImcGauge(
+                        imc: imcValue,
+                      );
+                    }),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  controller: pesoEC,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(labelText: 'Peso'),
+                  inputFormatters: [
+                    CurrencyTextInputFormatter(
+                        locale: 'pt_BR',
+                        symbol: '',
+                        turnOffGrouping: true,
+                        decimalDigits: 2)
+                  ],
+                ),
+                TextFormField(
+                  controller: alturaEC,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(labelText: 'Altura'),
+                  inputFormatters: [
+                    CurrencyTextInputFormatter(
+                        locale: 'pt_BR',
+                        symbol: '',
+                        turnOffGrouping: true,
+                        decimalDigits: 2)
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      var formValid = formKey.currentState?.validate() ?? false;
+
+                      if (formValid) {
+                        var formatter = NumberFormat.simpleCurrency(
+                            locale: 'pt_BR', decimalDigits: 2);
+
+                        double peso = formatter.parse(pesoEC.text) as double;
+                        double altura =
+                            formatter.parse(alturaEC.text) as double;
+
+                        _calcularIMC(peso: peso, altura: altura);
+                      }
+                    },
+                    child: const Text('Calcular IMC'))
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
